03a2995c2db58af4b4dbc59cbf707db6f0fb0dbc
Cesar Aparicio <contacto@cesaraparicio.es>
2018-04-18 17:38:52 +0200

    feat: almost finish

--- userData.js
+++ userData.js
@@ -2,76 +2,69 @@ const Twitter = require('twitter');
 require('dotenv').config();
 
 const client = new Twitter({
-  consumer_key: process.env.consumer_key,
-  consumer_secret: process.env.consumer_secret,
-  access_token_key: process.env.access_token_key,
-  access_token_secret: process.env.access_token_secret
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token_key: process.env.access_token_key,
+    access_token_secret: process.env.access_token_secret
 });
 
-const params = {screen_name: 'ecesaraparicio'};
-
-const retrieveUserFollowers = () => (
-  new Promise(resolve => (
-    client.get(
-      'followers/list',
-      params,
-      (error, followers, response) => {
-        if (!error) {
-          const followersData = followers.users.map(user => (
-               {
-                 screen_name: user.screen_name,
-                 name: user.name,
-                 description: user.description,
-                 followers_count: user.followers_count,
-                 friends_count: user.friends_count,
-               }
-           ));
-           resolve(followersData);
-        } else {
-          resolve([]);
-        }
-      }
-    )
-  ))
+const retrieveUserFollowers = usr => (
+    new Promise(resolve => (
+        client.get(
+            'followers/list',
+            {screen_name: usr},
+            (error, followers, response) => {
+                if (!error) {
+                    const followersData = followers.users.map(user => (
+                        {
+                            screen_name: user.screen_name,
+                            name: user.name,
+                            description: user.description,
+                            followers_count: user.followers_count,
+                            friends_count: user.friends_count,
+                        }
+                    ));
+                    resolve(followersData);
+                } else {
+                    console.log(error);
+                    resolve([]);
+                }
+            }
+        )
+    ))
 );
 
 const retrieveUserTweets = (screen_name) => (
-  new Promise(resolve => (
-    client.get(
-      'statuses/user_timeline',
-      { screen_name },
-      (error, tweets, response) => {
-        if (!error) {
-          const userTweets = tweets.map(tweet => tweet.text)
-          resolve(userTweets);
-        } else {
-          resolve([]);
-        }
-      }
-    )
-  ))
+    new Promise(resolve => (
+        client.get(
+            'statuses/user_timeline',
+            { screen_name },
+            (error, tweets, response) => {
+                if (!error) {
+                    const userTweets = tweets.map(tweet => tweet.text)
+                    resolve(userTweets);
+                } else {
+                    resolve([]);
+                }
+            }
+        )
+    ))
 );
 
-async function retrieveUserData() {
-  const followersData = await retrieveUserFollowers();
-  const followersTweets = followersData.map(follower => {
-    return retrieveUserTweets(follower.screen_name);
-  });
-  const followerDataWithTweets = await Promise.all(followersTweets).then(tweets => (
-    followersData.map((follower, i) => (
-      {
-        ...follower,
-        tweets: tweets[i],
-      }
+async function retrieveUserData(usr) {
+    const followersData = await retrieveUserFollowers(usr);
+    const followersTweets = followersData.map(follower => {
+        return retrieveUserTweets(follower.screen_name);
+    });
+    const followerDataWithTweets = await Promise.all(followersTweets).then(tweets => (
+        followersData.map((follower, i) => (
+            {
+                ...follower,
+                tweets: tweets[i],
+            }
+        ))
     ))
-  ))
-  const filterItems = query => {
-    return followerDataWithTweets.filter(el =>
-    el.toLowerCase().indexOf(query.toLowerCase()) > -1
-  );
-  }
-  // console.log(filterItems('SEO'));
-  return followerDataWithTweets
+    return followerDataWithTweets
 };
 
 // PARA SELECCIONAR KEYWORDS DEL ARRAY
@@ -97,10 +90,51 @@ async function retrieveUserData() {
 // console.log(followerDataWithTweets.indexOf(SEO));
 
 
-  retrieveUserData()
+var usuario = 'ecesaraparicio'
+//var usuario = 'sicodeandres '
+retrieveUserData(usuario)
     .then(userData => {
-      console.log(userData)
-    })
+        if (userData.length > 0) {
+            var totales = userData.length
+            const filterItems = query => {
+                return userData.filter(el =>
+                    el.description.toLowerCase().indexOf(query.toLowerCase()) > -1 || el.tweets.filter(t => t.toLowerCase().indexOf(query.toLowerCase()) > -1).length > 0
+                );
+            }
+            var filtrados = filterItems('SEO');
+            var reales = filtrados.length;
+            var myscore = (totales - reales)/totales;
+            //console.log(userData)
+            //console.log("My score: "+myscore);
+
+            var res = userData.map( fllwr => {
+                //console.log("Recuperando followers de "+fllwr.screen_name)
+                return retrieveUserData(fllwr.screen_name).then(ud => {
+                    var totales = ud.length
+                    const filterItems = query => {
+                        return ud.filter(el =>
+                            el.description.toLowerCase().indexOf(query.toLowerCase()) > -1 || el.tweets.filter(t => t.toLowerCase().indexOf(query.toLowerCase()) > -1).length > 0
+                        );
+                    }
+                    var filtrados = filterItems('SEO');
+                    var reales = filtrados.length;
+                    var fllw_score = reales/totales;
+                    return fllw_score;
+                })
+            });
+            Promise.all(res).then(values => {
+                var v = values.filter(Boolean);
+                var sum_scores = v.reduce(function(valorAnterior, valorActual, indice, vector){
+                    return valorAnterior + valorActual;
+                });
+                var score_total = sum_scores/v.length * myscore
+                console.log("Score total del usuario "+usuario+": "+score_total);
+            })
+        } else {
+            console.log("No hay datos del usuario "+usuario)
+        }
+    }
+    )
     .catch(error => {
-      console.log(error)
+        console.log(error)
     })
